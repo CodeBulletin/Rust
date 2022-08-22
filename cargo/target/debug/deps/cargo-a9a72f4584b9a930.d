e16845c8a20893f888d62ba72bafb9ae072266ab
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-08-22 10:16:25 +0530

    Delete cargo/target directory

--- cargo/target/debug/deps/cargo-a9a72f4584b9a930.d
+++
@@ -1,5 +0,0 @@
-c:\Users\bmalh\Desktop\Projects\Rust\cargo\target\debug\deps\cargo-a9a72f4584b9a930.rmeta: src\main.rs
-
-c:\Users\bmalh\Desktop\Projects\Rust\cargo\target\debug\deps\cargo-a9a72f4584b9a930.d: src\main.rs
-
-src\main.rs:
